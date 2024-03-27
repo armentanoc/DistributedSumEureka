a8cdc4a155ea9bc04636ad38e0c1388d2cf3222b
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-27 15:55:45 -0300

    feat: app a controller and global exception handler.

---
+++ eureka-client-a/src/main/java/br/ucsal/armentanoc/eurekaclienta/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package br.ucsal.armentanoc.eurekaclienta;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        ex.printStackTrace();
+        String errorMessage = "Erro: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
+}
--- eureka-client-a/src/main/java/br/ucsal/armentanoc/eurekaclienta/controller/AppAController.java
+++ eureka-client-a/src/main/java/br/ucsal/armentanoc/eurekaclienta/controller/AppAController.java
@@ -5,14 +5,12 @@ import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Applications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,13 +18,50 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-public class HealthCheckController {
+public class AppAController {
     @Autowired
     @Lazy
     private EurekaClient eurekaClient;
 
     @Value("${spring.application.name}")
     private String appName;
+
+    @GetMapping("/getRandomSumFromAppB")
+    private Double getRandomSumFromAppB() {
+
+        InstanceInfo instance = checkIfAppBExists();
+        HttpRequest request;
+        HttpResponse<String> response;
+
+        try {
+            request = HttpRequest.newBuilder()
+                    .uri(new URI("http://" + instance.getIPAddr() + ":" + instance.getPort() + "/sumWithRandomFromAppC"))
+                    .GET()
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    
+        try {
+            response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            Double sum = Double.parseDouble(responseBody);
+            System.out.println("Soma gerada na App B chegou na App A: "+sum);
+            return sum;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private InstanceInfo checkIfAppBExists() {
+        String idAppB = "ME003501.mshome.net:eureka-client-b:8082";
+        List<InstanceInfo> instances = eurekaClient.getInstancesById(idAppB);
+        if (instances.isEmpty()) 
+            throw new RuntimeException("AppB not found");
+        return instances.get(0);
+    }
+
+    /////////////////////////////////////////////////
 
     @GetMapping("/health")
     public String healthy() {
